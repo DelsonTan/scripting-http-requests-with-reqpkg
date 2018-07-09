13ac93e7af7a298bc9975b885e1d1685a585e073
Delson Tan <delsonwtan@gmail.com>
2018-07-09 20:55:06 +0000

    Completed activity, downloaded image in folder

--- request.js
+++ request.js
@@ -1,2 +1,17 @@
 var request = require('request');
 var fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')
+       .on('error', function(err) {
+        throw err;
+       })
+       .on('response', function(response) {
+        console.log('Response Status Code: ', response.statusCode + '\n'
+          + 'Response Message: ' + response.statusMessage + '\n'
+          + 'Content Type: ' + response.headers['content-type'] + '\n'
+          + 'Download running ... ');
+       })
+       .on('end', function() {
+        console.log('DOwnload complete')
+      })
+      .pipe(fs.createWriteStream('./future.jpg'));
